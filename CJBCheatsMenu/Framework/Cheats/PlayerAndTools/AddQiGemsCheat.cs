586b4befb631229f7b41dbec62d706d1fbacefc9
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2026-03-07 12:05:57 -0500

    update for Stardew Valley 1.6.16

--- CJBCheatsMenu/Framework/Cheats/PlayerAndTools/AddQiGemsCheat.cs
+++ CJBCheatsMenu/Framework/Cheats/PlayerAndTools/AddQiGemsCheat.cs
@@ -32,6 +32,6 @@ internal class AddQiGemsCheat : BaseCheat
     /// <param name="amount">The amount to add.</param>
     private void AddGems(int amount)
     {
-        Game1.player.addItemToInventoryBool(new Object("858", amount));
+        Game1.player.TryAddToInventory(ItemRegistry.Create("(O)858", amount));
     }
 }
